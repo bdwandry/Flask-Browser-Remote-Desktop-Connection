2ecc7685f5e6719a43d42d647898d3c51bc24b81
Bryan Wandrych <bwandrych@Bryans-MBP.attlocal.net>
2022-11-05 12:29:18 -0400

    update

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,11 +4,32 @@ import {Button} from "@mui/material";
 import Get_Picture from "./components/get_picture";
 
 function App() {
-  return (
-    <div className="App">
-        <Get_Picture/>
-    </div>
-  );
+    const [start, setStart] = React.useState("");
+    async function get_pic() {
+        setStart("Start")
+    }
+
+    const start_picture = () => {
+        if (start === "") {
+            return (
+                <div>
+                    <Button variant="contained" onClick={get_pic}>Get Picture</Button>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <Get_Picture/>
+                </div>
+            )
+        }
+    }
+
+    return (
+        <div className="App">
+            {start_picture()}
+        </div>
+    );
 }
 
 export default App;
--- Client/src/components/get_picture.js
+++ Client/src/components/get_picture.js
@@ -1,32 +1,10 @@
 import React, {useEffect} from "react";
-import {Button} from "@mui/material";
 import axios from "axios";
 import "./image.css"
 
 
 const Get_Picture = () => {
     const [picture, setPicture] = React.useState("");
-    const handlePicture= (event) => {
-        setPicture(event.target.value);
-    };
-
-    const render_picture = () => {
-        if (picture !== "") {
-            console.log(picture.Image[0])
-            return(
-                <div>
-                    <img class="center-fit" src={picture.Image[0]} alt="Picture"/>
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    <Button variant="contained" onClick={get_pic}>Get Picture</Button>
-                </div>
-            )
-        }
-    }
-
     async function get_pic() {
         axios.post('http://127.0.0.1:5000/get_picture', {
             "Picture": "Send Picture",
@@ -37,6 +15,23 @@ const Get_Picture = () => {
             console.log(error);
         });
     }
+
+
+    const render_picture = () => {
+        if (picture !== "") {
+            console.log(picture.Image[0])
+            return(
+                <div>
+                    <img class="center-fit" src={picture.Image[0]} alt="Picture"/>
+                </div>
+            )
+        }
+    }
+
+    useEffect(() => {
+        const timer = setInterval(() => get_pic(), 1000);
+        return () => clearInterval(timer);
+    },[]);
 
     return(
         <div>
