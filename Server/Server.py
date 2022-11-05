102e1ece6973b2e39af5e4ff44657ff0e21e0d97
Bryan Wandrych <bwandrych@Bryans-MBP.attlocal.net>
2022-11-05 12:30:01 -0400

    update

--- Server/Server.py
+++ Server/Server.py
@@ -14,7 +14,6 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/get_picture', methods=['POST'])
 def send_email():
     received_message = request.get_json()
-    print(received_message)
     buffer = io.BytesIO()
     im = ImageGrab.grab()
     im.save(buffer, format='PNG')
@@ -22,8 +21,6 @@ def send_email():
     b64_str = base64.b64encode(buffer.getvalue())
     b64_str = str(b64_str).replace("b'", "").replace("'", "")
     b64_str = "data:image/PNG;base64," + b64_str
-    print(b64_str)
-    # time.sleep(5)
     return {"Image": [str(f"{b64_str}")]}
 
 
